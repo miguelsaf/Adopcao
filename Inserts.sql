5059b8b4ad6706666d07c5a9d6964fe92b626c9f
miguelsaf <miguelarcanjo03@gmail.com>
2024-03-08 15:04:46 +0100

    Inserts das tabelas da BD

---
+++ Inserts.sql
@@ -0,0 +1,18 @@
+insert into tb_municipio(municipio)
+values ('luanda'), ('belas'), ('cacuaco'), ('cazenga'), ('icolo e bengo'), ('Quiçama'), ('kilamba kiaxi'), ('talatona'), ('Viana');
+
+insert into tb_tipo_de_animal (tipo_de_animal)
+values ('cao'), ('gato');
+
+insert into tb_estado_da_adopcao (estado_da_adopcao)
+values ('aprovado'), ('nao aprovado'), ('pendete'), ('devolvido');
+
+insert into tb_disponibilidade_do_animal (disponibilidade_do_animal)
+values ('adoptado'), ('nao adoptado'), ('em adopcao');
+
+insert into tb_cor (cor)
+values ('castanho'), ('preto'), ('branco com pintas pretas'), ('cinza');
+
+insert into tb_genero (genero) 
+values ('F'), ('M');
+
